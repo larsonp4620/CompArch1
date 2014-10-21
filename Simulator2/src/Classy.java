032aee45f65039ca4cec00daa985b3bdd376aba3
Peter Larson <peterlsm4620@gmail.com>
2014-10-21 19:14:35 -0400

    THis is another commit

---
+++ Simulator2/src/Classy.java
@@ -0,0 +1,20 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author larsonp.
+ *         Created Oct 21, 2014.
+ */
+public class Classy {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		System.out.println("Hi");
+	}
+
+}
