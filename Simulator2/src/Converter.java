9118a37e3caafeb6b975a5f44cc4dd75fd7b6c3a
Peter Larson <peterlsm4620@gmail.com>
2014-10-21 20:36:52 -0400

    Fixed the converter to always give out 16 bit numbers.

--- Simulator2/src/Converter.java
+++ Simulator2/src/Converter.java
@@ -1,28 +1,35 @@
-
 /**
  * TODO Put here a description of what this class does.
- *
- * @author larsonp.
- *         Created Oct 21, 2014.
+ * 
+ * @author larsonp. Created Oct 21, 2014.
  */
 public class Converter {
 
 	/**
-	 * This converts from a string to an int. don't screw up. 
+	 * This converts from a string to an int. don't screw up.
+	 * 
 	 * @param input
 	 * @return
 	 */
-	public static int booleanString2Dec(String input){
+	public static int booleanString2Dec(String input) {
 		return Integer.parseInt(input);
 	}
-	
+
 	/**
 	 * This converts from a integer to a string. Don't mess up.
-	 *
+	 * 
 	 * @param i
 	 * @return
 	 */
-	public static String intToBooleanString(int i){
-		return Integer.toBinaryString(i);
+	public static String intToBooleanString(int i) {
+		String zeroes = "";
+		String temp = Integer.toBinaryString(i);
+		if (temp.length() < 16) {
+			int needed = 16 - temp.length();
+			for (int j = 0; j < needed; j++) {
+				zeroes=zeroes+"0";
+			}
+		}
+		return zeroes+temp;
 	}
 }
