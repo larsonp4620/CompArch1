28ff5d3408f791d8760536f22e0b5a81d83e91f7
Jacob Knispel <knispeja@rose-hulman.edu>
2014-10-21 21:02:21 -0400

    Wrote a class to simulate block memory

---
+++ Simulator2/src/Memory.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+
+/**
+ * Should represent the block memory of the processor
+ * 
+ * @author knispeja
+ *		   Created 10/21/2014
+ */
+public class Memory extends Object{
+	
+	private int[] memArray;
+	private int minAddr;
+	private int maxAddr;
+	
+	private int readAddr;
+	private int writeAddr;
+	private int din;
+	private String readval;
+	
+	private boolean memwrite;
+	
+	/**
+	 * Constructor initializes memory at default values
+	 */
+	public Memory(){
+		this(0, 32);
+	}
+	
+	/**
+	 * Constructor takes the minimum and maximum addresses (in decimal) to be used for memory
+	 * @param min
+	 * @param max
+	 */
+	public Memory(int min, int max){
+		this.memArray = new int[max - min];
+		Arrays.fill(this.memArray, -17);
+		this.minAddr = min;
+		this.maxAddr = max;
+		this.memwrite = false;
+	}
+	
+	public void clockCycle(){
+		if(this.readAddr >= this.minAddr && this.readAddr <= this.maxAddr)
+			this.readval = Converter.intToBooleanString(this.memArray[this.readAddr]);
+		else
+			System.err.println("Memory: Something passed an invalid address into readAddr");
+		
+		if(this.memwrite){
+			if(this.writeAddr >= this.minAddr && this.readAddr <= this.maxAddr)
+				this.memArray[this.writeAddr] = this.din;
+			else
+				System.err.println("Memory: Something passed an invalid address into writeAddr");
+		}
+			
+	}
+	
+	public void din(String data16bit){
+		this.din = Converter.booleanString2Dec(data16bit);
+	}
+	
+	public void readAddr(String addr16bit){
+		this.readAddr = Converter.booleanString2Dec(addr16bit);
+	}
+	
+	public void writeAddr(String addr16bit){
+		this.writeAddr = Converter.booleanString2Dec(addr16bit);
+	}
+	
+	public String read(){
+		return this.readval;
+	}
+	
+}
