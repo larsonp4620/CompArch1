97a878d1801432874ceced131cf607f67cbfcd26
Peter Larson <larsonp@rose-hulman.edu>
2014-10-25 16:47:13 -0400

    Added both input functions to the assembler

--- Simulator2/src/compiler/Assembler.java
+++ Simulator2/src/compiler/Assembler.java
@@ -1,6 +1,7 @@
 package compiler;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -8,10 +9,19 @@ public class Assembler {
 	boolean bytecodesuccess;
 	Scanner scan = null;
 	HashMap<String, String[]> byteMap = new HashMap<String, String[]>();
+	String[] assemblyInput;
+	String[] flags;
+	int instructionInterval;
 
-	public Assembler(String BytecodeS0urce) {
+	public Assembler(String BytecodeS0urce, int instructionInterval) {
+		this.instructionInterval=instructionInterval;
+		this.loadByteDefinitions(BytecodeS0urce);
+	}
+
+	private void loadByteDefinitions(String source) {
+
 		try {
-			File byteFile = new File(BytecodeS0urce);
+			File byteFile = new File(source);
 			this.scan = new Scanner(byteFile);
 			while (this.scan.hasNext()) {
 				String token1 = scan.next();
@@ -28,18 +38,59 @@ public class Assembler {
 			this.bytecodesuccess = false;
 		}
 	}
-	public String[] getAssemblerSet(){
-		String[] toReturn=new String[this.byteMap.size()];
-		int i=0;
-		for(String s:this.byteMap.keySet()){
-			toReturn[i]=s;
+
+	public String[] getAssemblerSet() {
+		String[] toReturn = new String[this.byteMap.size()];
+		int i = 0;
+		for (String s : this.byteMap.keySet()) {
+			toReturn[i] = s;
 			i++;
 		}
-		return toReturn;	
+		return toReturn;
 	}
 
-	public void run(String source, String destination) {
+	private void loadAssembly(String source) {
+		ArrayList<String> rawAssembly = new ArrayList<String>();
+		try {
+			File assemblyFile = new File(source);
+			this.scan = new Scanner(assemblyFile);
+			//this.scan.useDelimiter("\\s\n\\s|\\s:\\s");
+			String line="";
+			Scanner lineScanner;
+			
+			while (this.scan.hasNext()){
+				line=this.scan.nextLine();
+				if(line.contains(":")){
+					lineScanner=new Scanner(line);
+					rawAssembly.add(lineScanner.next());
+					if(lineScanner.hasNext())
+					rawAssembly.add(lineScanner.next());
+					if(lineScanner.hasNext())
+					rawAssembly.add(lineScanner.next());
+					lineScanner.close();
+				} else if(line.length()!=0){
+					rawAssembly.add(line);
+				}
+			}
+					
+			this.assemblyInput=rawAssembly.toArray(new String[2]);
+		} catch (Exception e) {
+			System.err
+					.println("Did not load assembly code correctly.\nThere could be a syntax error");
+		}
+	}
 
+	private void extractFlags(){
+		String temp;
+		for(int i=0;i<assemblyInput.length;i++){
+			temp=assemblyInput[i];
+			if(temp.contains(":")){
+				int position=temp.indexOf(":");
+			}
+		}
+	}
+	public void run(String source, String destination) {
+		this.loadAssembly(source);
 	}
 
 }
--- Simulator2/src/compiler/MainCompile.java
+++ Simulator2/src/compiler/MainCompile.java
@@ -2,8 +2,12 @@ package compiler;
 
 public class MainCompile {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		Assembler as1 = new Assembler("ByteSourceCode.txt");
+		Assembler as1 = new Assembler("src/compiler/ByteSourceCode.txt",2);
+		as1.run("src/compiler/test1assembly.txt", "destination");
+		System.out.println();
+		
 
 	}
 
