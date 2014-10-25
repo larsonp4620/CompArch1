bfe32585d4c8fe3bd0f452536164112b5fd0a655
Peter Larson <larsonp@rose-hulman.edu>
2014-10-25 14:38:07 -0400

    Modified the RegFile to support the new interfaces Writeable, and Readable.

--- Simulator2/src/RegFile.java
+++ Simulator2/src/RegFile.java
@@ -1,11 +1,13 @@
 import java.util.Random;
 
 /**
- * r0 is zero register
+ * Write id: 0-raddr1 1-raddr2 2-waddr 3-din 4-regWrite
+ * 
+ * Read Id: 0-dout1 1-dout2
  * 
  * @author larsonp. Created Oct 21, 2014.
  */
-public class RegFile implements Clockable {
+public class RegFile implements Clockable, Readable, Writable {
 	private int[] registerList;
 	Random randElement;
 	private int dout1;
@@ -29,40 +31,40 @@ public class RegFile implements Clockable {
 		int din = -13;
 		int regWrite = -13;
 
-		this.registerList[0]=0;
+		this.registerList[0] = 0;
 		for (int i = 1; i < 8; i++) {
 			registerList[i] = this.randElement.nextInt(65535);
 		}
 
 	}
 
-	public void setwrite1(String s) {
-		this.raddr1 = Converter.booleanString2Dec(s);
-	}
-
-	public void setwrite2(String s) {
-		this.raddr2 = Converter.booleanString2Dec(s);
-	}
-
-	public void setwriteW(String s) {
-		this.waddr = Converter.booleanString2Dec(s);
-	}
-
-	public void setdatain(String s) {
-		this.din = Converter.booleanString2Dec(s);
-	}
-
-	public void setRegWrite(String s) {
-		this.regWrite = Converter.booleanString2Dec(s);
-	}
-
-	public String getData1() {
-		return Converter.intToBooleanString(this.dout1);
-	}
-
-	public String getData2() {
-		return Converter.intToBooleanString(this.dout2);
-	}
+//	public void setwrite1(String s) {
+//		this.raddr1 = Converter.booleanString2Dec(s);
+//	}
+//
+//	public void setwrite2(String s) {
+//		this.raddr2 = Converter.booleanString2Dec(s);
+//	}
+//
+//	public void setwriteW(String s) {
+//		this.waddr = Converter.booleanString2Dec(s);
+//	}
+//
+//	public void setdatain(String s) {
+//		this.din = Converter.booleanString2Dec(s);
+//	}
+//
+//	public void setRegWrite(String s) {
+//		this.regWrite = Converter.booleanString2Dec(s);
+//	}
+//
+//	public String getData1() {
+//		return Converter.intToBooleanString(this.dout1);
+//	}
+//
+//	public String getData2() {
+//		return Converter.intToBooleanString(this.dout2);
+//	}
 
 	@Override
 	public void clockCycle() {
@@ -71,11 +73,53 @@ public class RegFile implements Clockable {
 			System.err
 					.println("Error: In the register file. "
 							+ "an imput to regFile was unspeficied. \nCycle not completed");
+
+		this.dout1 = this.registerList[this.raddr1];
+		this.dout2 = this.registerList[this.raddr2];
+
+		if (this.regWrite == 1 && this.waddr != 0)
+			this.registerList[this.waddr] = this.din;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Readable#read(int)
+	 */
+	@Override
+	public String read(int id) {
+		switch (id) {
+		case (0):
+			return Converter.intToBooleanString(this.dout1);
+		case (1):
+			return Converter.intToBooleanString(this.dout2);
+		default:
+			System.err.println("Invalide id value to the Register File");
+			return "";
+		}
+
+	}
+
+	@Override
+	public void write(int id, String data) {
+		switch(id){
+		case(0):
+			this.raddr1=Converter.booleanString2Dec(data);
+			return;
+		case(1):
+			this.raddr2=Converter.booleanString2Dec(data);
+			return;
+		case(2):
+			this.waddr=Converter.booleanString2Dec(data);
+			return;
+		case(3):
+			this.din=Converter.booleanString2Dec(data);
+			return;
+		case(4):
+			this.regWrite=Converter.booleanString2Dec(data);
+			return;
 		
-		this.dout1=this.registerList[this.raddr1];
-		this.dout2=this.registerList[this.raddr2];
-		
-		if(this.regWrite==1&&this.waddr!=0)
-			this.registerList[this.waddr]=this.din;
+		}
+
 	}
 }
